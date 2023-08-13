16422913604432b41f44a989f3a9501a3b8b0f32
Seong Heung Bae <mopil1102@gmail.com>
2023-08-14 00:13:48 +0900

    [SUL-2] 네비게이션 바 제작

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,23 +1,71 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+    ),
+    Text(
+      'Index 1: Feeds',
+    ),
+    Text(
+      'Index 2: Profile',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  _getIcon(String name) => Image.asset("assets/images/$name.png",
+      width: 30, height: 30, color: Colors.black);
+
+  @override
   Widget build(BuildContext context) {
+    String home = "home";
+    String feeds = "feeds";
+    String profile = "profile";
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("여기는 홈 화면 입니다."),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "여기는 홈 화면 입니다.",
-            ),
-          ],
-        ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: _getIcon(home),
+            label: home,
+            backgroundColor: Colors.white,
+          ),
+          BottomNavigationBarItem(
+            icon: _getIcon(feeds),
+            label: feeds,
+            backgroundColor: Colors.white,
+          ),
+          BottomNavigationBarItem(
+            icon: _getIcon(profile),
+            label: profile,
+            backgroundColor: Colors.white,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
       ),
     );
   }
